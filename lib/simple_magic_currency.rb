673b63f432b0a0984e9e9fbcc0415b2c1b7e732a
Fábio Araújo <fabioaraujo121@gmail.com>
2020-08-15 16:48:54 -0300

    solving default issue

--- lib/simple_magic_currency.rb
+++ lib/simple_magic_currency.rb
@@ -1,50 +1,52 @@
-require "simple_magic_currency/version"
+require 'simple_magic_currency/version'
 
 module SimpleMagicCurrency
-	class Error < StandardError; end
-	BRL = {delimiter: ".", separator: ",", unit: "R$", precision: 2, position: "before"}
-	USD = {delimiter: ',', separator: ".", unit: "US$", precision: 2, position: "before"}
-    BTC = {delimiter: ',', separator: ".", unit: "BTC", precision: 6, position: "after"}
+  class Error < StandardError; end
 
-  	module Number
-  		def to_currency(currency = nil, options = {})
-  			number = self
-  			default   = currency.nil? ? SimpleMagicCurrency::DEFAULT : currency
-  			options   = default.merge(options)
-  			precision = options[:precision] || default[:precision]
-  			unit      = options[:unit] || default[:unit]
-  			position  = options[:position] || default[:position]
-  			separator = precision > 0 ? options[:separator] || default[:separator] : ""
-  			delimiter = options[:delimiter] || default[:delimiter]
+  BRL = {delimiter: '.', separator: ',', unit: 'R$', precision: 2, position: 'before'}
+  USD = {delimiter: ',', separator: '.', unit: 'US$', precision: 2, position: 'before'}
+  BTC = {delimiter: ',', separator: '.', unit: 'BTC', precision: 6, position: 'after'}
 
-  			begin
-  				parts = number.with_precision(precision).split('.')
-  				number = parts[0].to_i.with_delimiter(delimiter) + separator + parts[1].to_s
-  				position == "before" ? unit + " " + number : number + " " + unit
-  			rescue 
-  				number
-  			end
-  		end
+  module Number
+    def to_currency(currency = nil, options = {})
+      number    = self
+      default   = currency.nil? ? BRL : currency
+      options   = default.merge(options)
+      precision = options[:precision] || default[:precision]
+      unit      = options[:unit] || default[:unit]
+      position  = options[:position] || default[:position]
+      separator = precision > 0 ? options[:separator] || default[:separator] : ''
+      delimiter = options[:delimiter] || default[:delimiter]
 
-  		def with_delimiter(delimiter=",", separator=".")
-  			number = self
-  			begin
-  				parts = number.to_s.split(separator)
-  				parts[0].gsub!(/(\d)(?=(?:\d{3})+(?!\d))/, "\\1#{delimiter}")
-  				parts.join separator
-  			rescue
-  				self
-  			end
-  		end
+      begin
+        parts = number.with_precision(precision).split('.')
+        number = parts[0].to_i.with_delimiter(delimiter) + separator + parts[1].to_s
+        position == 'before' ? unit + ' ' + number : number + ' ' + unit
+      rescue 
+        number
+      end
+    end
 
-  		def with_precision(precision=3)
-  			number = self
-  			"%01.#{precision}f" % number
-  		end
-  	end
+    def with_delimiter(delimiter=',', separator='.')
+      number = self
+      begin
+        parts = number.to_s.split(separator)
+        parts[0].gsub!(/(\d)(?=(?:\d{3})+(?!\d))/, "\\1#{delimiter}")
+        parts.join separator
+      rescue
+        self
+      end
+    end
+
+    def with_precision(precision=3)
+      number = self
+      "%01.#{precision}f" % number
+    end
+  end
 end
 
 class Integer; include SimpleMagicCurrency::Number; end
 class Float; include SimpleMagicCurrency::Number; end
 class Decimal; include SimpleMagicCurrency::Number; end
 class BigDecimal; include SimpleMagicCurrency::Number; end
+class String; include SimpleMagicCurrency::Number; end
